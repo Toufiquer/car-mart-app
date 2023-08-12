c5143a541daed6f4866c2127d199fcc37daabd08
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-08-13 01:31:28 +0600

    done @ setup tailwindcss and config

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,7 @@
-import CardsContainer from "./CardsContainer";
-import Hero from "./Hero";
-import Offer from "./Offer";
-
 const Home = () => {
   return (
     <div className="container mx-auto">
-      <Hero />
-      <CardsContainer />
-      <Offer />
+      <div className="text-center">Hello</div>
     </div>
   );
 };
